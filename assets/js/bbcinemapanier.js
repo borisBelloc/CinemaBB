4ec82a5ad14350cf306a04e1ec46506b1fb0137a
Boris BELLOC <bb.itwork@gmail.com>
2019-11-05 10:52:02 +0100

    premiere version select generated

--- assets/js/bbcinemapanier.js
+++ assets/js/bbcinemapanier.js
@@ -4,14 +4,64 @@
 //     $('#select').append($('<option></option>').val(p).html(p));
 // });
 
+
+
+// -------- Creation SELECT
+// !
 var tarifs = ["Plein tarif", "- 14 ans"];
 $('#panierSelectTarifs').empty();
-$.each(tarifs, function(i, p) {
-    $('#panierSelectTarifs').append($('<option></option>').val(p).html(p));
+$.each(tarifs, function (i, p) {
+  $('#panierSelectTarifs').append($('<option></option>').val(p).html(p));
 });
 
-// var ttt = {"Plein tarif": 100, id2: 200, "tag with spaces": 300};
-// $('#panierSelectTarifs').val()
+var seances = ["du lundi au vendredi de 9h à 18h", "du lundi au vendredi en soirée", "le week-end"];
+$('#panierSeances').empty();
+$.each(seances, function (i, p) {
+  $('#panierSeances').append($('<option></option>').val(p).html(p));
+});
+
+// -------- BASKET
+var countBasket = 0;
+
+$('#incrementBasket').click(function () {
+  countBasket += 1;
+  $("#caae").text(countBasket);
+
+  // $("#oklm").append($('<p class="mb-0">').text("NEW LINE"));
+  $("#_oklm").append($(
+    '<select id="basketNumber' +countBasket+'" class="changeableSelect">',
+    '<option>choix tarifs</option>',
+    '</select>'
+    ));
+    $.each(tarifs, function (i, p) {
+      $('#basketNumber'+countBasket).append($('<option></option>').val(p).html(p));
+    });
+    $('#_oklm').append("<br/>")
+
+
+  // $('.oklm').empty();
+  // $.each(tarifs, function (i, p) {
+  //   $('.oklm').append($(
+
+  //     '<select id="" class="changeableSelect">',
+  //     '<option>choix tarifs</option>',
+  //     '<option></option>',
+  //     '</select>'
+      
+  //     ).val(p).html(p));
+  // });
+
+
+  /*
+  <select id="panierDeux" class="changeableSelect">
+    <option>choix tarifs</option>
+  </select>
+
+
+  */
+
+});
+
 
 
 // Valeur à l'ouverture de page :
@@ -22,22 +72,18 @@ $.each(tarifs, function(i, p) {
 // * surveille seulement 1 id
 // $("#panierSelectTarifs").change(function() {
 
-// * surveille tous les 'changeresponsibleuser'
-$(document).on('change', '.changeableSelect',function(){
+// * surveille tous les 'changeableSelect'
+$(document).on('change', '.changeableSelect', function () {
 
   // * Recup la value selected: String
   // console.log($("#panierSelectTarifs").val());
   // console.log($("#panierSelectTarifs").index());
-  
+
   var tarifName = $("#panierSelectTarifs").val();
   console.log("tarifName val() " + tarifName);
 
   // var listItem = $("#panierSelectTarifs");
   // console.log( "Index: " + $( "option" ).index( listItem ) );
-
-  // Select index of value
-  // console.log($("#panierSelectTarifs").prop('selectedIndex'));
-  // TODO: utiliser variable a la place de l'id ou faire un switch case si pas possible
   var indexFirst = $("#panierSelectTarifs").prop('selectedIndex');
   console.log("indexFirst > " + indexFirst);
   var indexSecond = $("#panierSeances").prop('selectedIndex');
@@ -45,25 +91,15 @@ $(document).on('change', '.changeableSelect',function(){
 
   varQuantity = $("#quantityPlacePanier").val();
   console.log(varQuantity);
-  
+
   // var index = $( "#panierSelectTarifs" ).index( this );
   // $( "#calculTarif" ).text( "That was div index #" + index );
-  
-  var priceList = [[10, 12, 12.50],[5, 6, 6]]
-  $( "#calculTarif" ).text( "Prix unitaire : " + priceList[indexFirst][indexSecond] );
-  $( "#calculTarifTotal" ).text( "Prix total : " + (priceList[indexFirst][indexSecond]*varQuantity) );
-  
+
+  var priceList = [
+    [10, 12, 12.50],
+    [5, 6, 6]
+  ]
+  $("#calculTarif").text("Prix unitaire : " + priceList[indexFirst][indexSecond]);
+  $("#calculTarifTotal").text("Prix total : " + (priceList[indexFirst][indexSecond] * varQuantity));
+
 });
-
-// Tableau de tableau prix
-// [type tarif][horaire]
-// console.log("AAA " + priceList[1][1]);
-
-
-
-var seances = ["du lundi au vendredi de 9h à 18h", "du lundi au vendredi en soirée", "le week-end"];
-$('#panierSeances').empty();
-$.each(seances, function(i, p) {
-    $('#panierSeances').append($('<option></option>').val(p).html(p));
-});
-
